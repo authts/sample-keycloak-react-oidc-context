cf10cdef372f73c52093193263f7986c3ccfa064
zach-betz-hln <zachary.betz@hln.com>
2024-04-12 21:05:39 -0500

    vite server strictPort

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite';
+import { UserConfigExport, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command }) => {
+  const commonConfig: UserConfigExport = {
+    plugins: [react()],
+    base: './'
+  };
+
+  if (command === 'serve') {
+    return {
+      ...commonConfig,
+      server: {
+        strictPort: true
+      }
+    };
+  }
+
+  // command === 'build'
+  return {
+    ...commonConfig
+  };
 });
