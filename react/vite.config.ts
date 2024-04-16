280c893759a767965f6ad589c2b8b937f1e4881c
zach-betz-hln <zachary.betz@hln.com>
2024-04-16 00:20:30 -0500

    vite proxy not needed

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,10 +1,8 @@
-import { UserConfigExport, defineConfig, loadEnv } from 'vite';
+import { UserConfigExport, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(({ command }) => {
   const commonConfig: UserConfigExport = {
     plugins: [react()],
     base: '/'
@@ -15,13 +13,7 @@ export default defineConfig(({ command, mode }) => {
       ...commonConfig,
       server: {
         open: true,
-        strictPort: true,
-        proxy: {
-          [`${env.VITE_AUTHORITY_PATH}`]: {
-            target: `http://localhost:${env.VITE_AUTHORITY_PORT}`,
-            changeOrigin: true
-          }
-        }
+        strictPort: true
       }
     };
   }
