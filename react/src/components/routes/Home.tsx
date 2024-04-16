9e88bd205482867402a94591756c5631201c7094
zach-betz-hln <zachary.betz@hln.com>
2024-04-16 01:00:05 -0500

    show user

--- react/src/components/routes/Home.tsx
+++ react/src/components/routes/Home.tsx
@@ -1,8 +1,19 @@
+import styled from '@emotion/styled';
+import { useAuth } from 'react-oidc-context';
+
+const Pre = styled.pre`
+  overflow: auto;
+  white-space: pre-wrap;
+`;
+
 const Home: React.FC = () => {
+  const auth = useAuth();
+
   return (
     <>
       <h1>Home</h1>
-      <p>TODO</p>
+
+      <Pre>{JSON.stringify(auth.user, null, 2)}</Pre>
     </>
   );
 };
