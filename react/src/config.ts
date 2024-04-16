46409aa36e360d57d8b8c6f7bd7b32aa77c100f4
zach-betz-hln <zachary.betz@hln.com>
2024-04-16 15:57:09 -0500

    Revert "move userManager to main"
    This reverts commit e058043184811983dedeafd4ee6e649bd8939de5.

---
+++ react/src/config.ts
@@ -0,0 +1,9 @@
+import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
+
+export const userManager = new UserManager({
+  authority: import.meta.env.VITE_AUTHORITY,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  redirect_uri: window.location.origin + window.location.pathname,
+  post_logout_redirect_uri: window.location.origin,
+  userStore: new WebStorageStateStore({ store: window.sessionStorage })
+});
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -5,15 +5,7 @@ import ProtectedApp from './components/ProtectedApp';
 import Layout from './components/Layout';
 import { AuthProvider } from 'react-oidc-context';
 import { BrowserRouter } from 'react-router-dom';
-import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
-
-export const userManager = new UserManager({
-  authority: import.meta.env.VITE_AUTHORITY,
-  client_id: import.meta.env.VITE_CLIENT_ID,
-  redirect_uri: window.location.origin + window.location.pathname,
-  post_logout_redirect_uri: window.location.origin,
-  userStore: new WebStorageStateStore({ store: window.sessionStorage })
-});
+import { userManager } from './config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
