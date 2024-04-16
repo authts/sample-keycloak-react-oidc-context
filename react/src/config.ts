b4c041c3ad8e982dbd2ad9dc90a85c4e3ab37048
zach-betz-hln <zachary.betz@hln.com>
2024-04-16 10:35:18 -0500

    bump access token lifespan to 5 mins

--- react/src/config.ts
+++ react/src/config.ts
@@ -7,3 +7,11 @@ export const userManager = new UserManager({
   post_logout_redirect_uri: window.location.origin + '/',
   userStore: new WebStorageStateStore({ store: window.sessionStorage })
 });
+
+userManager.events.addAccessTokenExpired(() => console.log('addAccessTokenExpired'));
+userManager.events.addAccessTokenExpiring(() => console.log('addAccessTokenExpiring'));
+userManager.events.addSilentRenewError(() => console.log('addSilentRenewError'));
+userManager.events.addUserLoaded(() => console.log('addUserLoaded'));
+userManager.events.addUserSessionChanged(() => console.log('addUserSessionChanged'));
+userManager.events.addUserSignedIn(() => console.log('addUserSignedIn'));
+userManager.events.addUserSignedOut(() => console.log('addUserSignedOut'));
