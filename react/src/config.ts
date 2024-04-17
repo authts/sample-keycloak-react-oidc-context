411c78cc27017e41b1f6bca5fc36e14dee154902
zach-betz-hln <zachary.betz@hln.com>
2024-04-16 19:54:17 -0500

    monitorSession

--- react/src/config.ts
+++ react/src/config.ts
@@ -3,7 +3,8 @@ import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
 export const userManager = new UserManager({
   authority: import.meta.env.VITE_AUTHORITY,
   client_id: import.meta.env.VITE_CLIENT_ID,
-  redirect_uri: window.location.origin + window.location.pathname,
+  redirect_uri: `${window.location.origin}${window.location.pathname}`,
   post_logout_redirect_uri: window.location.origin,
-  userStore: new WebStorageStateStore({ store: window.sessionStorage })
+  userStore: new WebStorageStateStore({ store: window.sessionStorage }),
+  monitorSession: true // this allows cross tab login/logout detection
 });
