ed4e5e948a7b37f3f801585b67bb722f380ac873
Zachary "Zach" Betz <119352232+zach-betz-hln@users.noreply.github.com>
2024-04-23 15:22:44 -0500

    fix util script flag for installing dev deps (#5)

--- api/task_log_command_to_upgrade_all_deps.mjs
+++ api/task_log_command_to_upgrade_all_deps.mjs
@@ -6,5 +6,5 @@ const json = readFileSync(filePath, { encoding: 'utf-8' });
 const obj = JSON.parse(json);
 const dependencies = Object.keys(obj['dependencies'] ?? {});
 const devDependencies = Object.keys(obj['devDependencies'] ?? {});
-const command = `npm install ${dependencies.join(' ')} && npm install ${devDependencies.join(' ')}`;
+const command = `npm install ${dependencies.join(' ')} && npm install --save-dev ${devDependencies.join(' ')}`;
 console.log(command);
