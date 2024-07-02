22266761466c4bc2e059051c7efbbbb97ccfa5fe
Zachary Betz <119352232+zach-betz-hln@users.noreply.github.com>
2024-07-01 22:22:06 -0500

    api: migrate to biome (#11)

--- api/task_log_command_to_upgrade_all_deps.mjs
+++ api/task_log_command_to_upgrade_all_deps.mjs
@@ -1,8 +1,8 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 
 const obj = JSON.parse(readFileSync(resolve('package.json'), { encoding: 'utf-8' }));
-const dependencies = Object.keys(obj['dependencies'] ?? {});
-const devDependencies = Object.keys(obj['devDependencies'] ?? {});
+const dependencies = Object.keys(obj.dependencies ?? {});
+const devDependencies = Object.keys(obj.devDependencies ?? {});
 const command = `npm i ${dependencies.join(' ')} && npm i -D ${devDependencies.join(' ')}`;
 console.log(command);
